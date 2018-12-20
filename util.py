e2203244d02328a0fa28b902ae53ed076a1f2eab
Maho <msasaki.a@gmail.com>
2018-12-20 13:27:49 -0600

    update readme and tag selection logic in the build

--- util.py
+++ util.py
@@ -43,6 +43,7 @@
 import os
 import os.path as path
 import shutil
+import re
 
 # returns the newest full tag (to the patch) given an abbreviated version (to minor version)
 # get_newest_tag("v3.2", ["v3.1.6", "v3.2.0", "v3.2.1", "v3.2.2", "v3.2.14", "v3.3.1", "v3.3.2-dev.5","v3.3.3"])
@@ -70,6 +71,37 @@ def get_newest_tag(given_tag, raw_tags):
                 # print("building version " + ver)
                 return ver
 
+
+def get_newest_doc_tag(given_tag, raw_tags):
+    doc_tags = []
+    doc_tags_cropped = []
+    for doc_tag in raw_tags:
+        m = re.search(r'.*-doc\d+$', doc_tag)
+        # if the doc_tag ends in -docs+digits m will be a Match object, or None otherwise.
+        if m is not None:
+            # print(m.group())
+            doc_tags_cropped.append(m.group().split("-")[0])
+            doc_tags.append(m.group())
+
+    # print(doc_tags)
+    # print(doc_tags_cropped)
+
+    # get_newest_tag(given_tag, doc_tags_cropped) gives the newest full tag (v3.2.18) for the specified tag (v3.2)
+
+    to_sort = [dtag for dtag in doc_tags if dtag.startswith(get_newest_tag(given_tag, doc_tags_cropped))]
+    # print(to_sort)
+    tag_nums = []
+    for d in to_sort:
+        m = re.search(r'\d+$', d)
+        if m is not None:
+            tag_num = int(m.group())
+            # print (m.group())
+            tag_nums.append(tag_num)
+    print(tag_nums)
+    for d in to_sort:
+        if d.endswith(str(max(tag_nums))):
+            # print(d)
+            return d  # v3.2.2-doc11
 
 def copy_contents(src_dir, dest_dir, skip_fpattern=None, skip_dpattern=None):
     for root, dirnames, filenames in os.walk(src_dir):
